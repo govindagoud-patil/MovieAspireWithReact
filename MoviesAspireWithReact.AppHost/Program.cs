ccda469a9569d109e3bbf652411d7fdaaf79afc5
govindagoud <govindagoud.patil@hotmail.com>
2024-02-19 14:57:32 -0800

    added Readme

--- MoviesAspireWithReact.AppHost/Program.cs
+++ MoviesAspireWithReact.AppHost/Program.cs
@@ -2,7 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var webapi = builder.AddProject<Projects.Movies_webapi>("webapi");
 
-var webapp = builder.AddNpmApp("react", "../webapp/react/movies")
+var webapp = builder.AddNpmApp("webapp", "../webapp/react/movies")
                     .WithReference(webapi)
                     .WithEndpoint(containerPort: 3000, scheme: "http", env: "PORT")
                     .AsDockerfileInManifest();
